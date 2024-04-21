612d6bd9d0b8c699309a8a07950baa21fed9c837
kaurvali <hullumeelne.vali@gmail.com>
2024-04-21 22:16:39 +0300

    First version of green calculator

---
+++ script.js
@@ -0,0 +1,131 @@
+const statuses = Object.freeze({
+    OK: 0,
+    MEDIUM: 1,
+    BAD: 2,
+});
+
+window.addEventListener('DOMContentLoaded', loadTranslations);
+
+let translations;
+
+function showResults() {
+    const language = document.querySelector('html').getAttribute('lang');
+    getResults(language);
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("results-box").style.display = "block";
+}
+
+function hideResults() {
+    let resultsSection = document.getElementsByClassName("result-section");
+    let resultsArray = Array.from(resultsSection)
+
+    resultsArray.forEach(function (element) {
+        element.style.display = 'none';
+    });
+    removeAllChildren("results")
+
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("results-box").style.display = "none";
+}
+
+async function loadTranslations() {
+    const response = await fetch('translations.json', { mode: 'cors' });
+    if (response.ok) {
+        translations = await response.json();
+        setLanguage('ee');
+    }
+}
+
+function setLanguage(language) {
+    document.querySelector('html').setAttribute('lang', language);
+    const elements = document.querySelectorAll('[data-i18n]');
+    elements.forEach(element => {
+        const key = element.getAttribute('data-i18n');
+        element.textContent = translations[language][key];
+    });
+    removeAllChildren("results")
+}
+
+function getResults(language) {
+    let issueTopics = checkResults(language);
+    let status = determineStatus(issueTopics, document.querySelectorAll('.question').length);
+
+    const resultsDiv = document.getElementById("results");
+    var text = document.createElement("p");
+
+    if (status === statuses.OK) {
+        document.getElementById("ok").style.display = "block";
+
+        text.textContent = translations[language].all_ok;
+        resultsDiv.appendChild(text);
+
+    } else if (status === statuses.MEDIUM) {
+        document.getElementById("medium").style.display = "block";
+
+        var text1 = document.createElement("p");
+        text1.textContent = translations[language].issues_with;
+        resultsDiv.appendChild(text1);
+
+        createList(issueTopics);
+
+        var text2 = document.createElement("p");
+        text2.textContent = translations[language].issues_with_followup
+        resultsDiv.appendChild(text2);
+    } else {
+        document.getElementById("bad").style.display = "block";
+
+        text.textContent = translations[language].you_should_work_harder;
+        resultsDiv.appendChild(text);
+    }
+}
+
+function checkResults(language) {
+    const questions = document.querySelectorAll(".question");
+
+    let issueTopics = [];
+    questions.forEach((question, index) => {
+        const checkboxes = question.querySelectorAll('input[type="checkbox"]');
+        const questionValue = question.getAttribute("value");
+        let countOk = 0;
+
+        checkboxes.forEach((checkbox) => {
+            if (checkbox.checked) countOk++;
+        });
+
+        if (countOk < 3) {
+            const translation = translations[language][questionValue];
+            issueTopics.push(translation.toLowerCase());
+        }
+    });
+    return issueTopics;
+}
+
+function determineStatus(issueTopics, questionLength) {
+    if (issueTopics.length === 0) return statuses.OK;
+    else if (issueTopics.length === questionLength) return statuses.BAD;
+    return statuses.MEDIUM;
+}
+
+function createList(issueTopics) {
+    var ul = document.createElement("ul");
+    issueTopics.forEach(function (item) {
+        var listItem = document.createElement("li");
+        listItem.textContent = item;
+        ul.appendChild(listItem);
+    });
+    var result = document.getElementById("results");
+    result.appendChild(ul);
+
+}
+
+function removeAllChildren(id) {
+    var container = document.getElementById(id);
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+function changeLanguage(language) {
+    setLanguage(language);
+    getResults(language)
+}
